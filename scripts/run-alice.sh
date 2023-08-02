358d731230f5739996e53844d64df1d7d37121cd
Mosaic Contributor 911ed3ac <noreply-contributor-911ed3ac@mosaicchain.io>
2023-08-02 11:07:03 +0000

    fix(scripts): removed telemetry url from testnet launch scripts

--- scripts/run-alice.sh
+++ scripts/run-alice.sh
@@ -2,4 +2,4 @@
 
 ./target/release/node-template purge-chain --base-path /tmp/alice --chain local -y
 
-./target/release/node-template --base-path /tmp/alice --chain local --alice --port 30333 --ws-port 9945 --unsafe-ws-external --rpc-port 9933 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --validator --rpc-methods=Unsafe --rpc-cors=all
+./target/release/node-template --base-path /tmp/alice --chain local --alice --port 30333 --ws-port 9945 --unsafe-ws-external --rpc-port 9933 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator --rpc-methods=Unsafe --rpc-cors=all
