c0f98a451ac5762691d95e6e948496fbe04eb9f9
Mosaic Contributor 911ed3ac <noreply-contributor-911ed3ac@mosaicchain.io>
2023-09-13 11:47:19 +0000

    feat(session-hook)!: hooks now return DispatchResult

--- utils/src/session_hook.rs
+++ utils/src/session_hook.rs
@@ -1,29 +1,42 @@
+use frame_support::pallet_prelude::DispatchResult;
 pub use sp_staking::SessionIndex;
 
 #[allow(unused)]
 pub trait SessionHook {
 	/// Called on genesis session
-	fn session_genesis(idx: SessionIndex) {}
+	fn session_genesis(idx: SessionIndex) -> DispatchResult {
+		Ok(())
+	}
 	/// Called when a session is planned
-	fn session_planned(idx: SessionIndex) {}
+	fn session_planned(idx: SessionIndex) -> DispatchResult {
+		Ok(())
+	}
 	/// Called when a session is started
-	fn session_started(idx: SessionIndex) {}
+	fn session_started(idx: SessionIndex) -> DispatchResult {
+		Ok(())
+	}
 	/// Called before session ends
-	fn session_ended(idx: SessionIndex) {}
+	fn session_ended(idx: SessionIndex) -> DispatchResult {
+		Ok(())
+	}
 }
 
 #[impl_trait_for_tuples::impl_for_tuples(16)]
 impl SessionHook for Tuple {
-	fn session_genesis(idx: SessionIndex) {
-		for_tuples!( #( Tuple::session_genesis(idx); )* );
+	fn session_genesis(idx: SessionIndex) -> DispatchResult {
+		for_tuples!( #( Tuple::session_genesis(idx)?; )* );
+		Ok(())
 	}
-	fn session_planned(idx: SessionIndex) {
-		for_tuples!( #( Tuple::session_planned(idx); )* );
+	fn session_planned(idx: SessionIndex) -> DispatchResult {
+		for_tuples!( #( Tuple::session_planned(idx)?; )* );
+		Ok(())
 	}
-	fn session_started(idx: SessionIndex) {
-		for_tuples!( #( Tuple::session_started(idx); )* );
+	fn session_started(idx: SessionIndex) -> DispatchResult {
+		for_tuples!( #( Tuple::session_started(idx)?; )* );
+		Ok(())
 	}
-	fn session_ended(idx: SessionIndex) {
-		for_tuples!( #( Tuple::session_ended(idx); )* );
+	fn session_ended(idx: SessionIndex) -> DispatchResult {
+		for_tuples!( #( Tuple::session_ended(idx)?; )* );
+		Ok(())
 	}
 }
