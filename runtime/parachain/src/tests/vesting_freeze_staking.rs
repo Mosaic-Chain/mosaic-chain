7cfa907a3fce4bbc6704ddcfa55053d3424ecebb
Mosaic Contributor a6286633 <noreply-contributor-a6286633@mosaicchain.io>
2025-10-16 20:01:04 +0200

    test(parachain): using named account instead of numbers in integration tests

--- runtime/parachain/src/tests/vesting_freeze_staking.rs
+++ runtime/parachain/src/tests/vesting_freeze_staking.rs
@@ -20,65 +20,65 @@ use utils::{
 #[test]
 fn vesting_schedule_converted_to_freeze_stays_frozen_after_unstake() {
 	new_test_ext().execute_with(|| {
-		let _ = Balances::deposit_creating(&100, 100000); // avoid overdominant stake
-		let _ = Balances::deposit_creating(&10, 1000);
-		assert_ok!(HoldVesting::add_vesting_schedule(&10, Schedule::new(500, 10, Some(1))));
+		let _ = Balances::deposit_creating(&BOB, 100000); // avoid overdominant stake
+		let _ = Balances::deposit_creating(&ALICE, 1000);
+		assert_ok!(HoldVesting::add_vesting_schedule(&ALICE, Schedule::new(500, 10, Some(1))));
 
-		assert_eq!(Balances::free_balance(10), 500);
-		assert_eq!(HoldVesting::vesting_balance(&10), Some(500));
+		assert_eq!(Balances::free_balance(ALICE), 500);
+		assert_eq!(HoldVesting::vesting_balance(&ALICE), Some(500));
 
-		assert_ok!(VestingToFreeze::convert_schedule(RawOrigin::Signed(10).into(), 0));
+		assert_ok!(VestingToFreeze::convert_schedule(RawOrigin::Signed(ALICE).into(), 0));
 
-		assert_eq!(Balances::free_balance(10), 1000);
-		assert_eq!(Balances::usable_balance(10), 500);
-		assert_eq!(HoldVesting::vesting_balance(&10), None);
+		assert_eq!(Balances::free_balance(ALICE), 1000);
+		assert_eq!(Balances::usable_balance(ALICE), 500);
+		assert_eq!(HoldVesting::vesting_balance(&ALICE), None);
 
 		let nft =
-			NftStakingHandler::<Test>::mint(&10, &pallet_nft_staking::PermissionType::DPoS, &0)
+			NftStakingHandler::<Test>::mint(&ALICE, &pallet_nft_staking::PermissionType::DPoS, &0)
 				.expect("could mint permission nft");
 
-		assert_ok!(NftStaking::bind_validator(RawOrigin::Signed(10).into(), nft));
-		assert_ok!(NftStaking::self_stake_currency(RawOrigin::Signed(10).into(), 999));
+		assert_ok!(NftStaking::bind_validator(RawOrigin::Signed(ALICE).into(), nft));
+		assert_ok!(NftStaking::self_stake_currency(RawOrigin::Signed(ALICE).into(), 999));
 
-		assert_eq!(Balances::free_balance(10), 1);
-		assert_eq!(Balances::total_balance_on_hold(&10), 999);
-		assert_eq!(Balances::usable_balance(10), 0); // The ED is 1
+		assert_eq!(Balances::free_balance(ALICE), 1);
+		assert_eq!(Balances::total_balance_on_hold(&ALICE), 999);
+		assert_eq!(Balances::usable_balance(ALICE), 0); // The ED is 1
 
 		skip_min_staking_period();
-		assert_ok!(NftStaking::self_unstake_currency(RawOrigin::Signed(10).into(), 999));
+		assert_ok!(NftStaking::self_unstake_currency(RawOrigin::Signed(ALICE).into(), 999));
 		next_session();
 
-		assert_eq!(Balances::free_balance(10), 1000);
-		assert_eq!(Balances::total_balance_on_hold(&10), 0);
-		assert_eq!(Balances::usable_balance(10), 500);
+		assert_eq!(Balances::free_balance(ALICE), 1000);
+		assert_eq!(Balances::total_balance_on_hold(&ALICE), 0);
+		assert_eq!(Balances::usable_balance(ALICE), 500);
 
 		// Enough time has passed to thaw schedule
-		assert_ok!(VestingToFreeze::thaw_expired(RawOrigin::Signed(10).into()));
+		assert_ok!(VestingToFreeze::thaw_expired(RawOrigin::Signed(ALICE).into()));
 
-		assert_eq!(Balances::free_balance(10), 1000);
-		assert_eq!(Balances::usable_balance(10), 1000);
+		assert_eq!(Balances::free_balance(ALICE), 1000);
+		assert_eq!(Balances::usable_balance(ALICE), 1000);
 	})
 }
 
 #[test]
 fn slashing_frozen_schedules() {
 	new_test_ext().execute_with(|| {
-		let _ = Balances::deposit_creating(&100, 100000); // avoid overdominant stake
-		let _ = Balances::deposit_creating(&10, 1000);
-		assert_ok!(HoldVesting::add_vesting_schedule(&10, Schedule::new(900, 10, Some(1))));
-		assert_ok!(VestingToFreeze::convert_schedule(RawOrigin::Signed(10).into(), 0));
+		let _ = Balances::deposit_creating(&BOB, 100000); // avoid overdominant stake
+		let _ = Balances::deposit_creating(&ALICE, 1000);
+		assert_ok!(HoldVesting::add_vesting_schedule(&ALICE, Schedule::new(900, 10, Some(1))));
+		assert_ok!(VestingToFreeze::convert_schedule(RawOrigin::Signed(ALICE).into(), 0));
 		let nft =
-			NftStakingHandler::<Test>::mint(&10, &pallet_nft_staking::PermissionType::DPoS, &0)
+			NftStakingHandler::<Test>::mint(&ALICE, &pallet_nft_staking::PermissionType::DPoS, &0)
 				.expect("could mint permission nft");
 
-		assert_ok!(NftStaking::bind_validator(RawOrigin::Signed(10).into(), nft));
-		assert_ok!(NftStaking::self_stake_currency(RawOrigin::Signed(10).into(), 900));
+		assert_ok!(NftStaking::bind_validator(RawOrigin::Signed(ALICE).into(), nft));
+		assert_ok!(NftStaking::self_stake_currency(RawOrigin::Signed(ALICE).into(), 900));
 
 		next_session();
 
 		// Slash delegated stake by 50% (450)
 		let offence = Offence {
-			offenders: vec![10],
+			offenders: vec![ALICE],
 			session: Session::current_index(),
 			slash_fraction: Perbill::from_percent(50),
 		};
@@ -87,31 +87,31 @@ fn slashing_frozen_schedules() {
 
 		next_session();
 
-		assert_eq!(Balances::total_balance(&10), 550);
-		assert_eq!(Balances::free_balance(10), 100);
-		assert_eq!(Balances::total_balance_on_hold(&10), 450);
-		assert_eq!(Balances::usable_balance(10), 0);
+		assert_eq!(Balances::total_balance(&ALICE), 550);
+		assert_eq!(Balances::free_balance(ALICE), 100);
+		assert_eq!(Balances::total_balance_on_hold(&ALICE), 450);
+		assert_eq!(Balances::usable_balance(ALICE), 0);
 
 		skip_min_staking_period();
 
-		assert_ok!(NftStaking::self_unstake_currency(RawOrigin::Signed(10).into(), 450));
+		assert_ok!(NftStaking::self_unstake_currency(RawOrigin::Signed(ALICE).into(), 450));
 		next_session();
 
 		assert_eq!(
 			Balances::balance_frozen(
 				&pallet_vesting_to_freeze::FreezeReason::VestingToFreeze.into(),
-				&10
+				&ALICE
 			),
 			900
 		);
-		assert_eq!(Balances::free_balance(10), 550);
-		assert_eq!(Balances::total_balance_on_hold(&10), 0);
-		assert_eq!(Balances::usable_balance(10), 0);
+		assert_eq!(Balances::free_balance(ALICE), 550);
+		assert_eq!(Balances::total_balance_on_hold(&ALICE), 0);
+		assert_eq!(Balances::usable_balance(ALICE), 0);
 
 		// Enough time has passed to thaw schedule
-		assert_ok!(VestingToFreeze::thaw_expired(RawOrigin::Signed(10).into()));
+		assert_ok!(VestingToFreeze::thaw_expired(RawOrigin::Signed(ALICE).into()));
 
-		assert_eq!(Balances::free_balance(10), 550);
-		assert_eq!(Balances::usable_balance(10), 550);
+		assert_eq!(Balances::free_balance(ALICE), 550);
+		assert_eq!(Balances::usable_balance(ALICE), 550);
 	})
 }
